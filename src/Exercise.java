f0434526ad2a7408b03c312dc2bbe676238ba262
you88311 <you88311@naver.com>
2022-07-23 11:26:27 +0900

    연습

--- src/Exercise.java
+++ src/Exercise.java
@@ -3,16 +3,8 @@ import java.util.*;
 public class Exercise {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<Integer>() {
-            {
-                add(1);
-                add(2);
-                add(3);
-                add(4);
-            }
-        };
-
-        System.out.println(list.stream().mapToInt(Integer::valueOf).sum());
-
+        Integer num = 1;
+        System.out.println(num.toString());
+        System.out.println(num.toString() instanceof String );
     }
 }
