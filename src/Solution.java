a8ba4deb645edea15cd0a37a22c369d810304458
you88311 <you88311@naver.com>
2022-07-31 15:00:00 +0900

    연습

--- src/Solution.java
+++ src/Solution.java
@@ -1,57 +1,53 @@
+
 import java.util.*;
 
 public class Solution {
-    public int connectedVertices(int[][] edges) {
+    public ArrayList<Integer[]> divideChocolateStick(int M, int N) {
         // TODO:
-        Queue<Integer[]> queue = new LinkedList<>();
-        int groupCnt = 0;
-        int size = 0;
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        int nx, ny, x, y;
-        Integer[] cur;
+        ArrayList<Integer> tmp = new ArrayList<>();
+        int gcd = 1;
+        int min = Math.min(M, N);
+        int max = Math.max(M, N);
+        int num;
 
-        for (int[] edge : edges) {
-            for (int i : edge) {
-                size = Math.max(size, i);
-            }
+        /*최대공약수 구하기*/
+        while (gcd != 0) {
+            gcd = max / min;
+            max = gcd;
         }
-        size += 1;
+        gcd = min;
 
-        int[][] graph = new int[size][size]; //0으로 초기화
-        boolean[][] visited;
-        for (int[] e : edges) {
-            //연결은1, 연결x는 0(기본값) , 방문했으면 -1
-            graph[e[0]][e[1]] = 1;
-            graph[e[1]][e[0]] = 1;
+        /*공약수 구하기*/
+        for (int i = 1; i <= Math.sqrt(gcd); i++) {
+            if (M % i == 0 && N % i == 0)
+                tmp.add(i);
         }
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (graph[i][j] != 1 || i == j) continue;
-                queue.add(new Integer[]{i,j});
-                graph[i][j] = -1;
-                graph[j][i] = -1;
-                while (!queue.isEmpty()) {
-                    cur = queue.poll();
-                    x = cur[0];
-                    y = cur[1];
-                    if (graph[x][y] == 1) {
-                        for (int k = 0; k < 4; k++) {
-                            nx = x + dx[k];
-                            ny = y + dy[k];
-                            if(nx < 0 || ny < 0 || nx >= size || ny >= size) continue;
-                            if (graph[nx][ny] == 1) {
-                                queue.add(new Integer[]{nx, ny});
-                                graph[nx][ny] = -1;
-                                graph[ny][nx] = -1;
-                            }
-                        }
-                    }
-                }
-                groupCnt++;
-            }
+        ArrayList<Integer> common = new ArrayList<>(tmp);
+        for(Integer e : tmp){
+            num = gcd/e;
+            if(e == Math.sqrt(gcd)) continue;
+            if(M % num == 0 && N % num == 0)
+                common.add(num);
         }
-        return groupCnt;
+
+        /*조합 return*/
+        ArrayList<Integer[]> result = new ArrayList<>();
+        for (Integer e : common) {
+            Integer[] arr = new Integer[]{e, M / e, N / e};
+            result.add(arr);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        ArrayList<Integer[]> result = sol.divideChocolateStick(4,8);
+        for(Integer[] arr : result){
+            System.out.println(Arrays.toString(arr));
+        }
     }
 }
+
