ee7ecd0185e44f706034159d24014f4e6fb75593
you88311 <you88311@naver.com>
2022-09-12 12:35:10 +0900

    연습장

--- src/Main.java
+++ src/Main.java
@@ -1,90 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static private Integer[] BFS(Queue<Integer[]> queue, Integer[] cur, int[][] maze, int[][] fire, int R, int C, boolean isFire) {
-        cur = queue.poll();
-        int[] dX = {1, -1, 0, 0};
-        int[] dY = {0, 0, 1, -1};
-        int x;
-        int y;
-        for (int i = 0; i < 4; i++) {
-            x = cur[0] + dX[i];
-            y = cur[1] + dY[i];
-            if (0 > x || x >= R || 0 > y || y >= C) continue;
-            if(maze[x][y] == -1) continue;
-            if(isFire){
-                if(fire[x][y] == 0) {
-                    fire[x][y] = fire[cur[0]][cur[1]] + 1;
-                    queue.add(new Integer[]{x,y});
-                }
-            }else {
-                if(maze[x][y] == 0) {
-                    if(fire[x][y]!= 0 && fire[x][y] > maze[cur[0]][cur[1]] + 1) {
-                        maze[x][y] = maze[cur[0]][cur[1]] + 1;
-                        queue.add(new Integer[]{x, y});
-                    }else if(fire[x][y] == 0){
-                        maze[x][y] = maze[cur[0]][cur[1]] + 1;
-                        queue.add(new Integer[]{x, y});
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        boolean visited[] = new boolean[n + 1];
+        LinkedList<Integer>[] graph = new LinkedList[n + 1];
+        for (int i = 0; i < n+1; i++) {
+            graph[i] = new LinkedList<Integer>();
+        }
+
+        Integer u, v;
+        int comCnt = 0;
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            u = Integer.valueOf(st.nextToken());
+            v = Integer.valueOf(st.nextToken());
+            //연결리스트로 그래프 저장
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        for (int i = 1; i < n+1; i++) {
+            if(visited[i]) continue;
+
+            queue.add(i);
+            visited[i] = true;
+            //bfs
+            while(!queue.isEmpty()){
+                int cur = queue.poll();
+
+                for (int adj : graph[cur]) {
+                    if(!visited[adj]){
+                        queue.add(adj);
+                        visited[adj] = true;
                     }
                 }
             }
+            comCnt++;
         }
 
-        return cur;
-    }
-
-    
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] arr = br.readLine().split(" ");
-        int R = Integer.parseInt(arr[0]);
-        int C = Integer.parseInt(arr[1]);
-
-        int[][] maze = new int[R][C];
-        int[][] fire = new int[R][C];
-        Queue<Integer[]> jQueue = new LinkedList<>();
-        Queue<Integer[]> fQueue = new LinkedList<>();
-        char c;
-        for (int i = 0; i < R; i++) {
-            arr = br.readLine().split("");
-            for (int j = 0; j < arr.length; j++) {
-                c = arr[j].charAt(0);
-                if (c == 'J') {
-                    jQueue.add(new Integer[]{i, j});
-                    maze[i][j] = 1;
-                } else if (c == 'F') {
-                    fQueue.add(new Integer[]{i, j});
-                    maze[i][j] = -1;
-                    fire[i][j] = 1;
-                } else if (c == '#') {
-                    maze[i][j] = -1;
-                } else
-                    maze[i][j] = 0;
-            }
-        }
-
-        Integer[] curJ = {0, 0};
-        Integer[] curF = {0, 0};
-        while (!fQueue.isEmpty()) {
-            BFS(fQueue, curF, maze, fire, R, C, true);
-        }
-
-        while (!jQueue.isEmpty()) {
-            curJ = BFS(jQueue, curJ, maze, fire, R, C, false);
-            if (curJ[0] == 0 || curJ[1] == 0
-                    || curJ[0] == R - 1 || curJ[1] == C - 1) {
-                System.out.print(maze[curJ[0]][curJ[1]]);
-                return;
-            }
-        }
-
-        System.out.print("IMPOSSIBLE");
-        br.close();
+        System.out.println(comCnt);
     }
 }
