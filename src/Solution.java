cb0f27111827309795f0c746ec9e51c5bd6308fa
you88311 <you88311@naver.com>
2022-09-17 10:05:05 +0900

    카카오 문제 연습

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,50 @@
 import java.util.*;
 
 public class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder(new_id);
 
-//
-//    scanner
-//    bufferedReader로 R,C 받고
-//    String[][] maze = new int[R][C];
-//
-//    for(i = ~ r)
-//    String[i] = readline()
-//
-//    int escapeTime = 0;
-//    while(J.queue is not empty)
-    //    escapeTime ++;
-    //    F => DFS하며 해당 위치를 #으로 변경
-    //    J => DFS
-    //    if(J의 좌표에 0이 있거나 R-1 or C-1이 있다면) return escapeTime
+        /*1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다*/
+        new_id = new_id.toLowerCase();
 
-//  return "
+        /*2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.*/
+        new_id = new_id.replaceAll("[^a-z0-9-_.]", "");
+
+        /*3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.*/
+        while (new_id.contains("..")) {
+            new_id = new_id.replaceAll("\\.{2}", "\\.");
+        }
+
+        /*4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.*/
+        if (new_id.charAt(0) == '.') new_id = new_id.substring(1);
+        if(new_id.length() != 0)
+            if (new_id.charAt(new_id.length() - 1) == '.') new_id = new_id.substring(0, new_id.length() - 1);
+
+        /*5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.*/
+        if (new_id.equals("")) new_id = "a";
+
+        /*6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+          만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.*/
+        if (new_id.length() >= 16) new_id = new_id.substring(0, 15);
+        if(new_id.length() != 0)
+            if (new_id.charAt(new_id.length() - 1) == '.') new_id = new_id.substring(0, new_id.length() - 1);
+
+        /*7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.*/
+        String lastCh = new_id.substring(new_id.length()-1);
+        while (new_id.length() <= 2){
+            new_id = new_id.concat(lastCh);
+        }
+
+            return new_id;
+    }
 
     public static void main(String[] args) {
-
+        Solution sol = new Solution();
+        System.out.println(sol.solution("...!@BaT#*..y.abcdefghijklm"));
+        System.out.println(sol.solution("z-+.^."));
+        System.out.println(sol.solution("=.="));
+        System.out.println(sol.solution("123_.def"));
+        System.out.println(sol.solution("abcdefghijklmn.p"));
     }
 }
